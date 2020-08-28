341d7dbbeacf4dc0eb9be82559ffab382bd317aa
ilhamday <ilhamyudha00@gmail.com>
2020-08-28 21:09:20 +0700

    udah nambah all_detail

---
+++ scrap.py
@@ -0,0 +1,27 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open('./res.html'), 'html.parser')
+
+cuisine = soup.find('h1', class_='search_title').string
+
+card = soup.find_all('div', class_='card search-snippet-card search-card')
+
+asso_cuisine = card.find('span', class_='col-s-11 col-m-12 nowrap pl0').text
+organisation = card.find('a', class_='result-title').text
+address = card.find('div', class_='search-result-address').text
+location = card.find('a', class_='search_result_subzone').text
+phone = card.find('a', class_='res-snippet-ph-info')['data-phone-no-str']
+
+# masukin character, contoh -> , Caroline Springs ke variable cut | dipakai buat split terus diambil item pertama yaitu addressnya
+cut = f', {location}'
+x = address.strip().split(cut)
+
+# strip() digunain untuk ngilangin spasi
+print(f'cuisine = {cuisine.strip()}')
+print(f'assosiation cuisine = {asso_cuisine}')
+print(f'organisation = {organisation.strip()}')
+print(f'address = {x[0]}')
+print(f'location = {location}')
+print(f'phone = {phone}')
